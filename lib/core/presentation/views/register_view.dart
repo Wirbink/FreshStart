20494b4d3126fcb6b9f5cb8ab3794f3bbfa46759
Irving <irvingramoncoyoltoliveros@gmail.com>
2024-06-09 17:29:35 -0500

    Fix RegisterView

--- lib/core/presentation/views/register_view.dart
+++ lib/core/presentation/views/register_view.dart
@@ -21,102 +21,99 @@ class _RegisterViewState extends State<RegisterView> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
-          height: screenHeight,
           decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('assets/images/LoginFreshBank.png'),
                 fit: BoxFit.cover),
           ),
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(height: screenHeight * 0.3),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                      horizontal: screenWidth * 0.1, vertical: 20.0),
-                  padding: const EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                    color: colorPanel,
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Registro",
-                        style: TextStyle(
-                            fontFamily: 'MarkPro',
-                            fontWeight: FontWeight.w900,
-                            fontSize: 30.0),
-                      ),
-                      const SizedBox(height: 24.0),
-                      const InputText(
-                          placeholder: "Nombre",
-                          iconPlaceholder: Icon(
-                            Icons.person,
-                            color: colorBorder,
-                          )),
-                      const SizedBox(height: 14.0),
-                      const InputText(
-                          placeholder: "Apellido",
-                          iconPlaceholder: Icon(
-                            Icons.person,
-                            color: colorBorder,
-                          )),
-                      const SizedBox(height: 14.0),
-                      const InputText(
-                          placeholder: "Email",
-                          iconPlaceholder: Icon(
-                            Icons.email_outlined,
-                            color: colorBorder,
-                          )),
-                      const SizedBox(height: 14.0),
-                      const InputText(
-                          placeholder: "Teléfono",
-                          iconPlaceholder: Icon(
-                            Icons.phone,
-                            color: colorBorder,
-                          )),
-                      const SizedBox(height: 14.0),
-                      const InputText(
-                          placeholder: "Nacimiento",
-                          iconPlaceholder: Icon(
-                            Icons.cake,
-                            color: colorBorder,
-                          )),
-                      const SizedBox(height: 14.0),
-                      const InputText(
-                          placeholder: "País",
-                          iconPlaceholder: Icon(
-                            Icons.apartment,
-                            color: colorBorder,
-                          )),
-                      const SizedBox(height: 14.0),
-                      const InputText(
-                          placeholder: "RFC",
-                          iconPlaceholder: Icon(
-                            Icons.pin,
-                            color: colorBorder,
-                          )),
-                      const SizedBox(height: 24.0),
-                      Center(
-                        child: GestureDetector(
-                          child: const GeneralButtonWidget(text: "Registrarse"),
-                        ),
-                      ),
-                      SizedBox(height: 8.0,),
-                      DividerWidget(),
-                      SizedBox(height: 8.0,),
-                      Center(
-                        child: GestureDetector(
-                          child: const GeneralButtonWidget(text: "Inicar Sesión"),
-                        ),
-                      ),
-                    ],
-                  ),
+          child: Column(
+            children: [
+              SizedBox(height: screenHeight * 0.3),
+              Container(
+                margin: EdgeInsets.symmetric(
+                    horizontal: screenWidth * 0.1, vertical: 20.0),
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: colorPanel,
+                  borderRadius: BorderRadius.circular(20.0),
                 ),
-              ],
-            ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Registro",
+                      style: TextStyle(
+                          fontFamily: 'MarkPro',
+                          fontWeight: FontWeight.w900,
+                          fontSize: 30.0),
+                    ),
+                    const SizedBox(height: 24.0),
+                    const InputText(
+                        placeholder: "Nombre",
+                        iconPlaceholder: Icon(
+                          Icons.person,
+                          color: colorBorder,
+                        )),
+                    const SizedBox(height: 14.0),
+                    const InputText(
+                        placeholder: "Apellido",
+                        iconPlaceholder: Icon(
+                          Icons.person,
+                          color: colorBorder,
+                        )),
+                    const SizedBox(height: 14.0),
+                    const InputText(
+                        placeholder: "Email",
+                        iconPlaceholder: Icon(
+                          Icons.email_outlined,
+                          color: colorBorder,
+                        )),
+                    const SizedBox(height: 14.0),
+                    const InputText(
+                        placeholder: "Teléfono",
+                        iconPlaceholder: Icon(
+                          Icons.phone,
+                          color: colorBorder,
+                        )),
+                    const SizedBox(height: 14.0),
+                    const InputText(
+                        placeholder: "Nacimiento",
+                        iconPlaceholder: Icon(
+                          Icons.cake,
+                          color: colorBorder,
+                        )),
+                    const SizedBox(height: 14.0),
+                    const InputText(
+                        placeholder: "País",
+                        iconPlaceholder: Icon(
+                          Icons.apartment,
+                          color: colorBorder,
+                        )),
+                    const SizedBox(height: 14.0),
+                    const InputText(
+                        placeholder: "RFC",
+                        iconPlaceholder: Icon(
+                          Icons.pin,
+                          color: colorBorder,
+                        )),
+                    const SizedBox(height: 24.0),
+                    Center(
+                      child: GestureDetector(
+                        child: const GeneralButtonWidget(text: "Registrarse"),
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    DividerWidget(),
+                    const SizedBox(height: 8.0),
+                    Center(
+                      child: GestureDetector(
+                        child: const GeneralButtonWidget(text: "Iniciar Sesión"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),

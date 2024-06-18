5dfa11debcae0703a1281dc7f19be8e3b70dbbe4
Brayn Sahagun <p.brayn.sahagun@go1.mx>
2024-06-18 05:11:18 -0500

    add statement_account bloc

---
+++ lib/core/presentation/bloc/StatementAccount/statement_account_event.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class StatementAccountEvent extends Equatable {
+  const StatementAccountEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class LoadStatementAccountDataEvent extends StatementAccountEvent {}
+
+class ClabeAccountChanged extends StatementAccountEvent {
+  final String clabeAccount;
+
+  const ClabeAccountChanged(this.clabeAccount);
+
+  @override
+  List<Object> get props => [clabeAccount];
+}
+
+class CardNumberAccountChanged extends StatementAccountEvent {
+  final String cardNumberAccount;
+
+  const CardNumberAccountChanged(this.cardNumberAccount);
+
+  @override
+  List<Object> get props => [cardNumberAccount];
+}
+
+class AmountAccountChanged extends StatementAccountEvent {
+  final double amountAccount;
+
+  const AmountAccountChanged(this.amountAccount);
+
+  @override
+  List<Object> get props => [amountAccount];
+}
+
+class AmountAccountSubmitted extends StatementAccountEvent {}
